5788efc6ec0b992b685322a1ab0cddaaa6547960
DharaneeswaranR <dharaneeshram@gmail.com>
2022-08-10 20:38:34 +0530

    Setup app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', async(event) => {
 
     const location = search.value
     
-    const response = await fetch(`http://localhost:3000/weather?address=${location}`)
+    const response = await fetch(`/weather?address=${location}`)
     const data = await response.json()
 
     if (data.error) {
